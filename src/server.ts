96499ee79ad28559cbe5b97a54cdd1ec27c40c80
mrkeylost <nikosam08@gmail.com>
2026-05-05 18:18:24 +0700

    feat: add server entry response

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,10 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Server is running", data: null });
+});
+
 app.use("/api", router);
 
 async function init() {
